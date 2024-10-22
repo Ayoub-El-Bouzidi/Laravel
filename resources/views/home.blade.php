95992aec43441fc7a238f10f81156b3e8e28bb4b
Ayoub <elbouzidiayoub.solicode@gmail.com>
2024-10-22 23:59:38 +0200

    agence immobilie

---
+++ resources/views/home.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Home</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg  bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <button class="btn btn-primary" type="submit">Contact Us</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <div>
+        <div>
+            <h1>Bienvenue chez <span class="text-info">Dar Ayoub</span></h1>
+        <p>Dar Ayoub est une agence immobilière reconnue pour son expertise et son <br>
+           engagement à fournir des solutions immobilières adaptées aux besoins de ses <br>
+           clients. Depuis notre création, nous avons aidé de nombreux acheteurs, vendeurs,<br>
+           investisseurs et locataires à trouver la propriété idéale. Que vous recherchiez un <br>
+           appartement moderne, une villa de luxe ou un terrain à construire, notre équipe est <br>
+           là pour vous accompagner à chaque étape.
+        </p>
+        </div>
+        <div id="image">
+            <img src="" alt="">
+        </div>
+      </div>
+</body>
+</html>
